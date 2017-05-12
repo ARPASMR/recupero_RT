0a16cdf1b76b666ba2f93e9a106ac45265c0bc77
meteo <meteo@localhost.localdomain>
2017-05-12 18:07:29 +0200

    modificato script per errori vari

--- launch_recuperoRT-new.sh
+++ launch_recuperoRT-new.sh
@@ -2,7 +2,7 @@
 #
 # lo script è all'interno del container e lancia in sequenza lo script R ogni 10 minuti
 # lo script fa sempre recuperi corti ogni 20'
-source ~/.bash_profile;
+#source ~/.bash_profile;
 #  s/S = short, ovvero recupero 1h
 #  l/L = long, ovvero recupero 24h
 #controllo se il container è già stato lanciato: se sì, ritardo l'esecuzione 
@@ -13,7 +13,7 @@ then
 else
   numsec=86400
 fi
-SECONDS=numsec+1
+SECONDS=$numsec+1
 LOCKFILE='usr/local/src/myscripts/.lock'
 
 while [ 1 ]
@@ -23,7 +23,7 @@ do
 #2.bis verifico che il servizio di collect sia online
 
 #3. lancio lo script
-if [ $SECONDS -ge numsec ]
+if [ $SECONDS -ge $numsec ]
 then
  Rscript Recupero_RT_v0.R
  SECONDS=0
